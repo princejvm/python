45fd70218512443ed7461dc5806a2af089f7884d
princejvm <sksinha.c@gmail.com>
2017-08-07 14:48:59 +0530

    simple program

---
+++ short_program.py
@@ -0,0 +1,12 @@
+# Any fool can write code that a computer can understand. Good programmers write code that humans can understand.
+#author-->princejvm
+
+name = raw_input("What is your name? ")
+quest = raw_input("What is your quest? ")
+color = raw_input("What is your favorite color? ")
+
+print "Ah, so your name is %s, your quest is %s, and your favorite color is %s." % (name, quest, color)
+
+my_string="one day i will become a good coder"
+print len(my_string)
+print my_string.upper()
