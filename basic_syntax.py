4111489c9e8f9cb6ea3485d69121d094a3358744
princejvm <sksinha.c@gmail.com>
2017-08-07 14:24:24 +0530

    basic syntax

---
+++ basic_syntax.py
@@ -0,0 +1,57 @@
+print "Welcome to Python!"
+# initialisation of variable without datatype
+my_variable = 10
+
+# Set the variables to the values listed in the instructions!
+my_int = 7
+my_float = 1.23
+my_bool = True
+
+# Change the value of my_int to 3 on line 8!
+my_int = 3
+
+# Here's some code that will print my_int to the console:
+print my_int
+
+"""" here we follow two-space indentation
+    four-space indentation can also be used """
+
+# a kind of function declaration with defination
+def  spam():
+    eggs = 12
+    return eggs
+print spam()
+
+spam = True
+eggs = False
+"""come now
+and follow my lead"""
+
+# Set count_to equal to the sum of two big numbers
+
+count_to=100+100
+
+print count_to
+
+#Set eggs equal to 100 using exponentiation on line 3!
+
+eggs = 10**2
+
+print eggs
+
+#Set spam equal to 1 using modulo on line 3!
+
+spam = 4 % 3
+
+print spam
+
+# syntax_final
+monty = True
+python = 1.234
+monty_python = python**2
+
+""" Variables, which store values for later use
+Data types, such as numbers and booleans
+Whitespace, which separates statements
+Comments, which make your code easier to read
+Arithmetic operations, including +, -, *, /, **, and % """
