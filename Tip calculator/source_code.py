4a098bc13733524ae8bde4f00d487e14690f0d1d
princejvm <sksinha.c@gmail.com>
2017-08-07 14:31:28 +0530

    source_code

---
+++ Tip calculator/source_code.py
@@ -0,0 +1,11 @@
+# Any fool can write code that a computer can understand. Good programmers write code that humans can understand.
+#author->princejvm
+
+# Assign the variable
+
+meal = 44.50
+tax = 6.75 / 100
+tip=15.0/100
+meal=meal+meal*tax
+total=meal+tip*meal
+print "%.2f"%total
